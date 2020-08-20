7cafd35532fa9b9df2bedfe9915592868e3aeedd
Blayne Chard <blayne@chard.com>
2020-08-20 12:35:49 +1200

    feat: simplify the failure

--- index.js
+++ index.js
@@ -3,18 +3,8 @@ const fs = require('fs');
 
 async function main() {
     const inputBuf = await fs.promises.readFile('./bug_tile.webp')
-
-    console.log('Png')
-    const png = await Sharp(inputBuf).extract({
-        top: 128,
-        left: 128,
-        width: 256,
-        height: 256,
-    }).png().toBuffer()
-    fs.writeFileSync('./ouptut.png', png);
     
-    await Sharp(png).toBuffer()
-    
+    // This dies
     console.log('WebP')
     const webp = await Sharp(inputBuf).extract({
         top: 128,
@@ -23,7 +13,6 @@ async function main() {
         height: 256,
     }).toBuffer()
 
-    // This dies
     await Sharp(webp).toBuffer()
 }
 
