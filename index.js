34949ff6129e2283a1c3cdd09af058a1ad72261d
Blayne Chard <blayne@chard.com>
2020-08-20 12:30:03 +1200

    feat: add more test cases, including working? png

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ const Sharp = require('sharp');
 const fs = require('fs');
 
 async function main() {
-    const inputBuf = fs.readFileSync('./bug_tile.webp')
-    const sharp = Sharp(inputBuf)
-    sharp.extract({
+    const inputBuf = await fs.promises.readFile('./bug_tile.webp')
+
+    console.log('Png')
+    const png = await Sharp(inputBuf).extract({
         top: 128,
         left: 128,
         width: 256,
         height: 256,
-    })
-    const webp = await sharp.toBuffer()
+    }).png().toBuffer()
+    fs.writeFileSync('./ouptut.png', png);
+    
+    await Sharp(png).toBuffer()
+    
+    console.log('WebP')
+    const webp = await Sharp(inputBuf).extract({
+        top: 128,
+        left: 128,
+        width: 256,
+        height: 256,
+    }).toBuffer()
+
+    // This dies
     await Sharp(webp).toBuffer()
 }
 
-main().catch(console.error)
+main().catch(e => {
+    console.error(e)
+    process.exit(1)
+})
